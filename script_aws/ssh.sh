25a77ea5e39a72f593eec4b1bd9ed1d4b7357354
pfcontino <pfcontino@gmail.com>
2019-10-29 22:29:07 +0100

    minor changes

--- script_aws/ssh.sh
+++ script_aws/ssh.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-KEY_POS="./myKey.pem"
+KEY_POS="/home/pier/Desktop/progetto sdcc/myKey.pem"
 NAME=$monitor*
 INSTANCE_DNS=$(aws ec2 describe-instances --filters Name=tag:Name,Values=$NAME --output text --query 'Reservations[*].Instances[*].PublicDnsName')
-ssh -i $KEY_POS ec2-user@$INSTANCE_DNS
+ssh -i "$KEY_POS" ec2-user@$INSTANCE_DNS
