3dbeec61d4be3a92f2bc65df9ea2ceb53acc1774
pfcontino <pfcontino@gmail.com>
2019-11-05 16:31:13 +0100

    Added scripts for deployment

--- script_aws/depency.sh
+++ script_aws/depency.sh
@@ -1,5 +1,27 @@
+#!/bin/bash
+
+KEY_POS="/home/pier/Desktop/progetto_sdcc/myKey.pem"
+NAMES=( "master" "worker-1" "worker-2" "worker-3" )
+
+
+for (( i=0; i<${#NAMES[@]}; i++ ));
+do
+
+INSTANCE_DNS=$(aws ec2 describe-instances --filters Name=tag:Name,Values=${NAMES[$i]} --output text \
+		--query 'Reservations[*].Instances[*].PublicDnsName')
+
+echo "SSH CONNECTION TO $NAMES"
+
+
+
+#connecting by ssh
+ssh  -o "StrictHostKeyChecking=no" -i "$KEY_POS" ec2-user@$INSTANCE_DNS "
+
+#launching depency
 sudo yum update -y
 sudo yum install git -y
-git clone https://github.com/cesto93/progettoSDCC
 sudo yum install -y golang
 go get -u github.com/aws/aws-sdk-go
+"
+
+done
