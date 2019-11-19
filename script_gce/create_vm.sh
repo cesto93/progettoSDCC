be19cbcf73772e59ffe94222391a1b98bbf78951
Lexie93 <ponzo93@gmail.com>
2019-11-19 23:34:29 +0100

    added metrics json file

--- script_gce/create_vm.sh
+++ script_gce/create_vm.sh
@@ -2,10 +2,16 @@
 #create google compute vm
 #argument: instance name
 
-gcloud beta compute --project=concise-faculty-246814 instances \
-create $1 --zone=us-central1-a --machine-type=f1-micro \
+PROJ="concise-faculty-246814"
+ZONE="us-central1-a"
+SERV="wordcount-service-account"
+MTYPE="f1-micro"
+HDD="10GB"
+
+gcloud beta compute --project=$PROJ instances \
+create $1 --zone=$ZONE --machine-type=$MTYPE \
 --subnet=default --network-tier=PREMIUM --maintenance-policy=MIGRATE \
---service-account=wordcount-service-account@concise-faculty-246814.iam.gserviceaccount.com \
+--service-account=$SERV@$MTYPE.iam.gserviceaccount.com \
 --scopes=https://www.googleapis.com/auth/cloud-platform --tags=http-server,https-server \
---image=debian-9-stretch-v20191014 --image-project=debian-cloud --boot-disk-size=10GB \
+--image=debian-9-stretch-v20191014 --image-project=debian-cloud --boot-disk-size=$HDD \
 --boot-disk-type=pd-standard --boot-disk-device-name=$1 --reservation-affinity=any
