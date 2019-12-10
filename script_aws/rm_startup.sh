8acc777c761b2a1e890baf48d93510a415bc686d
pfcontino <pfcontino@gmail.com>
2019-12-10 14:52:12 +0100

    modified script add_statup

--- script_aws/rm_startup.sh
+++ script_aws/rm_startup.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
-MONITOR="./startup/monitor.txt"
-MONITOR_B64="./startup/monitor_b64.txt"
 CONF_MONITOR=$(<../configuration/monitor.json)
 
 #importing configuration
 MONITOR_NAMES=( $(echo $CONF_MONITOR | jq -r '.aws[].name') )
 
-base64 $MONITOR > $MONITOR_B64
+base64 $SCRIPT > $SCRIPT_BASE64
 
 for (( i=0; i<${#MONITOR_NAMES[@]}; i++ ));
 do
 	INST=$(aws ec2 describe-instances --filters Name=tag:Name,Values=${MONITOR_NAMES[$i]}  --query 'Reservations[*].Instances[*]' | jq 'flatten')
 	ID_MONITOR[$i]=$(echo $INST | jq -r '.[].InstanceId')
-	aws ec2 modify-instance-attribute --instance-id ${ID_MONITOR[$i]} --attribute userData --value file://$MONITOR_B64
+	aws ec2 modify-instance-attribute --instance-id ${ID_MONITOR[$i]} --user-data ":"
 done
+
+
