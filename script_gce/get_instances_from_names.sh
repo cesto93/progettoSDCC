eb0dcb902409ae5902fde2772a27bf47dd27859d
Lexie93 <ponzo93@gmail.com>
2019-12-12 00:11:46 +0100

    configured prometheus for private IPs

--- script_gce/get_instances_from_names.sh
+++ script_gce/get_instances_from_names.sh
@@ -4,10 +4,11 @@
 INPUT_JSON_FILE="../configuration/instances_names.json"
 OUTPUT_JSON_FILE="../configuration/generated/instances.json"
 mapfile -t INSTANCES <<< $(jq -r '.[]' $INPUT_JSON_FILE)
+#gcloud compute instances describe ${INSTANCES[0]} --zone "us-central1-a" --format "json" | jq -s .
 echo $(for i in "${INSTANCES[@]}"
 do
 	gcloud compute instances describe $i --zone "us-central1-a" --format "json" \
-	| jq -s ".[].networkInterfaces[0].accessConfigs[0].natIP"
+	| jq -s ".[].networkInterfaces[0].networkIP"
 	#echo "$i"
 done) | jq -s '[{targets:[(.[] + ":9100")],labels:{job:"prometheus"}}]'>$OUTPUT_JSON_FILE
 
