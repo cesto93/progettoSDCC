6f7a862c17cb24eb583dab8c907f2642464d770c
pfcontino <pfcontino@gmail.com>
2019-12-13 17:32:21 +0100

    added script for monitoring gce at startup

---
+++ script_gce/add_startup.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+source ./conf/key.sh
+ZONE="us-central1-a"
+CONF=$(<../configuration/word_count.json)
+NAMES=( $(echo $CONF | jq -r '.gc[].name') )
+
+for (( i=0; i<${#NAMES[@]}; i++ ));
+do
+gcloud compute instances add-metadata ${NAMES[$i]} --zone=$ZONE --metadata startup-script=\
+"
+sudo $HOME_DIR/zookeeper/bin/zkServer.sh start
+sleep 8
+cd $HOME_DIR/go/src/progettoSDCC/bin
+./agent
+"
+done
